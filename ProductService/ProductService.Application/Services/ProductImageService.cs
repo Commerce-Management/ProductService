76e259331681ef0293ed4a7caebb7be93c6b886f
Ziya Hacili <magahajili@gmail.com>
2025-10-14 21:25:42 +0400

    Add ProductImage service

--- ProductService/ProductService.Application/Services/ProductImageService.cs
+++ ProductService/ProductService.Application/Services/ProductImageService.cs
@@ -1,6 +1,35 @@
-﻿namespace ProductService.Application.Services;
+﻿using Imagekit.Sdk;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using ProductService.Core.Interfaces;
 
-public class ProductImageService
+namespace ProductService.Application.Services;
+
+public class ProductImageService(IConfiguration configuration) : IProductImageService
 {
+    private readonly ImagekitClient _imagekitClient = new (configuration["ImageKit:PublicKey"], configuration["ImageKit:PrivateKey"], configuration["ImageKit:UrlEndPoint"]);
     
+    public async  Task<string> UploadProductImageAsync(IFormFile imageData)
+    {
+        if (imageData == null || imageData.Length == 0)
+            throw new ArgumentException("Image data is null or empty.", nameof(imageData));
+
+        using var memoryStream = new MemoryStream();
+        await imageData.CopyToAsync(memoryStream);
+        var fileBytes = memoryStream.ToArray();
+
+        var ob = new FileCreateRequest
+        {
+            file = fileBytes,
+            fileName = Guid.NewGuid().ToString()
+        };
+        
+        var result = await _imagekitClient.UploadAsync(ob);
+        return result.url;
+    }
+
+    public Task<bool> DeleteProductImageAsync(string imageUrl)
+    {
+        throw new NotImplementedException();
+    }
 }
