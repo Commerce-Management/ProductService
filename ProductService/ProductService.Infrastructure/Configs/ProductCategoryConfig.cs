6e47bdafb479e949d1c052b7aa03f55fe854577a
Ziya Hacili <magahajili@gmail.com>
2025-10-15 23:11:34 +0400

    add configs for entities

--- ProductService/ProductService.Infrastructure/Configs/ProductCategoryConfig.cs
+++ ProductService/ProductService.Infrastructure/Configs/ProductCategoryConfig.cs
@@ -1,6 +1,29 @@
-﻿namespace ProductService.Infrastructure.Configs;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductService.Core.Entities;
 
-public class ProductCategoryConfig
+namespace ProductService.Infrastructure.Configs;
+
+public class ProductCategoryConfig : IEntityTypeConfiguration<ProductCategory>
 {
-    
+    public void Configure(EntityTypeBuilder<ProductCategory> builder)
+    {
+        builder.HasKey(pc => pc.Id);
+
+        builder.HasIndex(pc => new { pc.ProductId, pc.CategoryId })
+            .IsUnique();
+
+        builder.Property(pc => pc.ProductId)
+            .HasColumnType("uuid")
+            .IsRequired();
+
+        builder.Property(pc => pc.CategoryId)
+            .HasColumnType("uuid")
+            .IsRequired();
+
+        builder.HasOne(pc => pc.Product)
+            .WithMany(p => p.ProductCategories)
+            .HasForeignKey(pc => pc.ProductId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
--- ProductService/ProductService.Infrastructure/Configs/ProductConfig.cs
+++ ProductService/ProductService.Infrastructure/Configs/ProductConfig.cs
@@ -1,6 +1,53 @@
-﻿namespace ProductService.Infrastructure.Configs;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductService.Core.Entities;
 
-public class ProductConfig
+namespace ProductService.Infrastructure.Configs;
+
+public class ProductConfig : IEntityTypeConfiguration<Product>
 {
-    
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Id)
+            .HasColumnName("ProductID");
+
+        builder.Property(e => e.Name)
+            .HasMaxLength(100)
+            .HasColumnType("text");
+
+        builder.Property(e => e.Description)
+            .HasMaxLength(500)
+            .HasColumnType("text");
+
+        builder.Property(e => e.Price)
+            .HasColumnType("decimal(10,2)");
+
+        builder.Property(p => p.StockQuantity)
+            .IsRequired()
+            .HasColumnType("integer");
+
+        // ImageUrls — теперь массив text[]
+        builder.Property(e => e.ImageUrls)
+            .IsRequired()
+            .HasColumnType("text[]");
+
+        builder.Property(p => p.DesignData)
+            .HasColumnType("jsonb")
+            .IsRequired(false);
+
+        builder.Property(p => p.PreviewImage)
+            .HasColumnType("text")
+            .IsRequired(false);
+
+        builder.Property(p => p.Status)
+            .HasMaxLength(50)
+            .HasColumnType("text")
+            .IsRequired(false);
+
+        builder.Property(p => p.UserId)
+            .IsRequired(false)
+            .HasColumnType("uuid");
+    }
 }
