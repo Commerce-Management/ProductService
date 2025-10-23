39286d39a73bd4aba720212f26fb79e244236a6f
Ziya Hacili <magahajili@gmail.com>
2025-10-23 19:10:10 +0400

    entity update and interface service

--- ProductService/ProductService.Core/Entities/Product.cs
+++ ProductService/ProductService.Core/Entities/Product.cs
@@ -14,12 +14,6 @@ public class Product : IEntity
 
     public string[] ImageUrls { get; set; } = Array.Empty<string>();
     
-    // Новые поля для 3D модели
-    //public string? DesignData { get; set; } // JSON с данными 3D модели
-    //public string? PreviewImage { get; set; } // Base64 превью
-    //public string? Status { get; set; } // Draft, Ordered
-    //public Guid? UserId { get; set; } // ID пользователя, создавшего дизайн
-    
     public Guid ShopId { get; set; } 
     
     public virtual ICollection<ProductCategory> ProductCategories { get; set; } = new List<ProductCategory>();
--- ProductService/ProductService.Core/Interfaces/IProductService.cs
+++ ProductService/ProductService.Core/Interfaces/IProductService.cs
@@ -4,18 +4,11 @@ namespace ProductService.Core.Interfaces;
 
 public interface IProductService
 {
-    // Task<IEnumerable<GetProductDto>> GetAllProductsAsync(Guid currentUserId, bool loadFullImages = false);
-    // Task<(IEnumerable<GetProductDto> Products, int TotalCount)> GetPaginatedProductsAsync(
-    //     Guid currentUserId,
-    //     int pageNumber,
-    //     int pageSize,
-    //     bool loadFullImages = false,
-    //     Guid? categoryId = null,
-    //     string? sortField = null,
-    //     string? sortDirection = "asc");
-    // Task<IEnumerable<GetProductDto>> SearchProduct(Guid currentUserId, string searchTerm, bool loadFullImages = false);
-    // Task<GetProductDto> GetProductByIdAsync(Guid currentUserId, Guid id, bool loadFullImages = true);
-    Task<GetProductDto> CreateProductAsync(CreateProductDto productDto);
-    // Task<bool> UpdateProductAsync(Guid currentUserId, Guid id, UpdateProductDto productDto);
-    // Task<bool> DeleteProductAsync(Guid currentUserId, Guid productId);
+    Task<IEnumerable<GetProductDto>> GetAllProductsAsync(int page);
+    Task<IEnumerable<GetProductDto>> SearchProduct(string parameter, int page);
+    Task<GetProductDto> GetProductByIdAsync(Guid productId);
+    Task<IEnumerable<GetProductDto>> GetAllShopProductsAsync(Guid shopId, int page);
+    Task<GetProductDto> CreateProductAsync(Guid userId, CreateProductDto productDto);
+    Task<bool> UpdateProductAsync(Guid userId, Guid productId, UpdateProductDto productDto);
+    Task<bool> DeleteProductAsync(Guid userId, Guid shopId, Guid productId);
 }
