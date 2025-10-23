69530651dddf19608b0e1f9f64e3f834a6f28228
Ziya Hacili <magahajili@gmail.com>
2025-10-23 19:11:07 +0400

    proto + dto

--- ProductService/ProductService.Shared/DTO/UpdateProductDto.cs
+++ ProductService/ProductService.Shared/DTO/UpdateProductDto.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
 namespace ProductService.Shared.DTO;
 
 public record UpdateProductDto(
+    [Required]
+    string ShopId,
+    
     string Name,
+    
     string Description,
+    
     decimal Price,
+    
     int StockQuantity, 
     
     IFormFile[]? NewImages,
